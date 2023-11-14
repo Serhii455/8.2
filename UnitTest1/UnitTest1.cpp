a8319b49ae42fa069cc355e837f428582ab94509
Serhii455 <serhii.maksymiv.ri.2023@lpnu.ua>
2023-11-14 15:50:32 +0200

    my commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Pr_8.2/8.2.cpp"
+
+using namespace std;
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			string str = "Hello 123";
+
+			int g = MinLength(str);
+
+			Assert::AreEqual(g, 3);
+		}
+	};
+}
